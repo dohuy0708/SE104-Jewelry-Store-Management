7d7a80f4f7a8d3a82572930c7ac10ad3d0c023f2
PhanTranTienHung22520523 <22520523@gm.uit.edu.vn>
2024-06-18 09:34:41 +0700

    home chart

--- Jewelry store management/VIEW/scrHome.xaml.cs
+++ Jewelry store management/VIEW/scrHome.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Jewelry_store_management.VIEWMODEL;
 
 namespace Jewelry_store_management.GUI
 {
@@ -23,6 +24,7 @@ namespace Jewelry_store_management.GUI
         public scrHome()
         {
             InitializeComponent();
+            DataContext = new scrHomeViewModel();
         }
     }
 }
