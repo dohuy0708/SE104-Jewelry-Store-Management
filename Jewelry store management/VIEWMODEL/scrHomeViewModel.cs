d72a8a3e21a4e9bfe8bade6f3664ea0f034d9494
dohuy0708 <dohuy22314@gmail.com>
2024-06-25 21:13:59 +0700

    update

--- Jewelry store management/VIEWMODEL/scrHomeViewModel.cs
+++ Jewelry store management/VIEWMODEL/scrHomeViewModel.cs
@@ -53,7 +53,7 @@ namespace Jewelry_store_management.VIEWMODEL
             AddOrderCommand = new RelayCommand(async _ => await AddOrderClick());
             AddProductCommand = new RelayCommand(async _ => await AddProClick());
             AddServiceOrderCommand = new RelayCommand(async _ => await AddServiceOrderClick());
-            ViewGoldPriceCommand = new RelayCommand(async _ => await ViewGoldPriceClick());
+            ViewGoldPriceCommand = new RelayCommand(async param => await ViewGoldPriceClick(param));
 
             SalesSeries = new SeriesCollection
             {
@@ -67,7 +67,7 @@ namespace Jewelry_store_management.VIEWMODEL
             Weeks = new List<string> { "Tuần 5", "Tuần 6", "Tuần 7", "Tuần 8", "Tuần 9", "Tuần 10", "Tuần 11" };
         }
 
-        private async Task ViewGoldPriceClick()
+        private async Task ViewGoldPriceClick(object parameter)
         {
           string url=parameter as string;
             if (!string.IsNullOrEmpty(url))
