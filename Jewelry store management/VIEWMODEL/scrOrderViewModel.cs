9f46276bd1f5ae51cdf18c99d43217ba2e009ecd
dohuy0708 <dohuy22314@gmail.com>
2024-06-26 11:39:33 +0700

    finish Order

--- Jewelry store management/VIEWMODEL/scrOrderViewModel.cs
+++ Jewelry store management/VIEWMODEL/scrOrderViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using Jewelry_store_management.HELPER;
 using Jewelry_store_management.MODELS;
@@ -18,6 +19,8 @@ namespace Jewelry_store_management.VIEWMODEL
         private readonly SaleOrderHelper _saleOrderHelper;
         public ICommand SearchCommand { get; set; }
         public ICommand AddOrderCommand { get; set; }
+        public ICommand ShowDetailCommand { get; set; }
+        public ICommand DeleteRowCommand { get; set; }
 
         private ObservableCollection<SaleOrder> orderEntries;
 
@@ -31,7 +34,18 @@ namespace Jewelry_store_management.VIEWMODEL
             }
         }
 
-     
+        private SaleOrder selectedSaleOrder;
+        public SaleOrder SelectedSaleOrder
+        {
+            get { return selectedSaleOrder; }
+            set
+            {
+                selectedSaleOrder = value;
+                OnPropertyChanged();
+
+            }
+        }
+
 
         public scrOrderViewModel()
         {
@@ -44,10 +58,78 @@ namespace Jewelry_store_management.VIEWMODEL
             // Khởi tạo lệnh tìm kiếm
             SearchCommand = new RelayCommand(Search);
            AddOrderCommand = new RelayCommand(async _ => await AddOrderClick());
+
+            DeleteRowCommand = new RelayCommand<SaleOrder>(async saleOrder => await DeleteRow(saleOrder));
+            ShowDetailCommand = new RelayCommand<SaleOrder>(ShowDetail);
+
+
             LoadAllSaleOrders();
 
 
         }
+
+
+
+        private async Task DeleteRow(SaleOrder saleOrder)
+        {
+            // Hiển thị thông báo xác nhận xóa
+            MessageBox_Window.ShowDialog("Bạn chắc chắn xóa đơn dịch vụ!", "Chú ý", "\\Drawable\\Icons\\icon_attention.png", MessageBox_Window.MessageBoxButton.OkCancel);
+
+            // Nếu người dùng chọn OK để xác nhận xóa
+            if (MessageBox_Window.buttonResultClicked == MessageBox_Window.ButtonResult.OK)
+            {
+                if (saleOrder != null)
+                {
+                    try
+                    {
+                        // Xóa đơn dịch vụ từ Firebase
+                        await _saleOrderHelper.DeleteSaleOrder(saleOrder.SaleId);
+
+                        // Xóa đơn dịch vụ khỏi danh sách trong ViewModel
+                        OrderEntries.Remove(saleOrder);
+
+                        // Hiển thị thông báo thành công
+                        MessageBox_Window.ShowDialog("Xóa đơn dịch vụ thành công!", "Thành công", "\\Drawable\\Icons\\icon_success.png", MessageBox_Window.MessageBoxButton.OK);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Hiển thị thông báo lỗi nếu việc xóa gặp lỗi
+                        MessageBox.Show($"Error deleting service order: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    // Hiển thị thông báo lỗi nếu `serviceOrder` là null
+                    MessageBox.Show("Error: Service order is null!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+        }
+
+
+
+        private void ShowDetail(SaleOrder saleOrder)
+        {
+            OrderEntries.Clear();
+
+            if (saleOrder != null)
+            {
+                var addSerView = new ReviewBill
+                {
+                    DataContext = new ReviewBillViewModel(saleOrder)
+                };
+                addSerView.ShowDialog();
+                SelectedSaleOrder = null;
+            }
+            else
+            {
+                MessageBox_Window.ShowDialog("Service order is null!", "Error", "\\Drawable\\Icons\\icon_error.png", MessageBox_Window.MessageBoxButton.OK);
+            }
+
+            LoadAllSaleOrders();
+        }
+
+
         private async void LoadAllSaleOrders()
         {
             var allSaleOrders = await _saleOrderHelper.GetAllSaleOrders();
