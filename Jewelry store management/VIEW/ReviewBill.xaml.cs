6d4a044518c98a399cc9805683245aa2656ecd40
dohuy0708 <dohuy22314@gmail.com>
2024-07-02 22:27:11 +0700

    commit

--- Jewelry store management/VIEW/ReviewBill.xaml.cs
+++ Jewelry store management/VIEW/ReviewBill.xaml.cs
@@ -23,5 +23,10 @@ namespace Jewelry_store_management.VIEW
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
